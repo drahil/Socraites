365a34e612b2521c8b60645bb8133b52a4edb34a
Omar Djecevic <djecevic.omar@gmail.com>
2025-04-26 17:09:34 +0200

    Initial commit

---
+++ src/Console/Application.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace drahil\Socraites\Console;
+
+use Symfony\Component\Console\Application as BaseApplication;
+
+class Application extends BaseApplication
+{
+    public function __construct()
+    {
+        parent::__construct('socraites', '0.0.1');
+
+        // Add commands here
+        // first one will be IndexCommand
+    }
+}
